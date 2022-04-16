8be84984d4e6245e0fb66452fe4c5fa6642c9c25
mnikn <mnikn@outlook.com>
2022-04-16 23:28:17 +0800

    feat: update ui

--- main.gd
+++ main.gd
@@ -3,8 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print_debug(self.get_viewport_rect().size)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$Player/AnimationPlayer.play("idle")
